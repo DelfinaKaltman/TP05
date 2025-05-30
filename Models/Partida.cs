f6a6ffb25f1e0e1bb87d254fd57743e3ffd88ad3
MILENA GALIT SVATZKY <49008545@ort>
2025-05-30 15:51:00 -0300

    avanzamos model

--- Models/Partida.cs
+++ Models/Partida.cs
@@ -5,58 +5,35 @@ using Newtonsoft.Json;
 
 namespace TP04.Models
 {
-    public class Ahorcado
+    public class Partida
     {
         [JsonProperty]
         public static string nombre { get; private set; }
         [JsonProperty]
-        public static string respuestaCorrecta { get; private set; }
+        public static Dictionary<string> respuestasCorrectas { get; private set; }
+        [JsonProperty]
+        public static int numeroSala { get; private set; }
+        [JsonProperty]
+        public static Dictionary<string> pistas { get; private set; }
 
-        [JsonProperty]
-        public static string respuesta { get; private set; }
-        [JsonProperty]
-        public static int sala { get; private set; }
         
-        public static void Sala1(string respuesta)
+        public Partida (string respuesta)
         {
-                respuestaCorrecta = "puerta principal";
-                respuesta = string.ToLower(respuesta);
-
-            if(respuesta == respuestaCorrecta){
-
-            }
-
-
-
-        }
-        public static string ArriesgarLetra(char letra)
-        {
-            string intentoPalabra = null;
-            letra = Char.ToLower(letra);
-            if(!letrasUsadas.Contains(letra))
-            {
-                letrasUsadas.Add(letra);
-                intentos++;
-            }
-            intentoPalabra = ArmarPalabra();
-	    return intentoPalabra;
+            respuestasCorrectas = new Dictionary<string>{"puerta principal", "inter", 3241, "MVC", "71612"};
+            sala = 0;
         }
 
-        public static string ArmarPalabra()
+        public static void Sala(string respuesta)
         {
-            string resultado = "";
-            foreach (char c in palabraElegida)
+            respuesta = string.ToLower(respuesta);
+            if (respuestasCorrectas.ContainsKey(numeroSala))
             {
-                if (letrasUsadas.Contains(c))
+                if (respuesta == respuestasCorrectas[numeroSala])
                 {
-                    resultado += c;
-                }
-                else
-                {
-                    resultado += "_";
+                    numeroSala++;
                 }
             }
-			return resultado;
         }
     }
 }
+
