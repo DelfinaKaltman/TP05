6acefe1626be553bf2b1bd5b56a28fa21e373a4f
MILENA GALIT SVATZKY <49008545@ort>
2025-06-19 09:04:07 -0300

    falta terminar de arreglar css

--- Models/Partida.cs
+++ Models/Partida.cs
@@ -20,7 +20,7 @@ namespace TP05.Models
         public Partida (string pNombre)
         {
             nombre = pNombre;
-            respuestasCorrectas = new List<string>{"puerta principal", "inter de milan", "3241", "MVC", "71612"};
+            respuestasCorrectas = new List<string>{"puerta principal", "inter de milan", "3421", "MVC", "71612"};
             numeroSala = 1;
             pistas = new List<string>{"Estoy al frente y todos deben pasar por mí. No soy camino… ¿qué soy?", "Lautaro Martínez se fue a este equipo, después de jugar en Racing", "Vestite de arriba para abajo", "Son tres letras, Delfi ya contó chistes sobre esto", "Las posiciones de las letras de GOL en el abecedario"};
         }
--- obj/Debug/net8.0/TP05.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP05.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP05")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44f0181138fac404e94a51f56d44964ddbcbf5d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d9e73134388f3691b638c41b8c13ccf2015171")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP05")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP05")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
